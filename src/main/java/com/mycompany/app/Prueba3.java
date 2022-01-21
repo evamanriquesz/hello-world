e464ff8ca280fc3f6508a6c6dc21509a24ee9faf
Eva Manrique <eva.manriquesz@gmail.com>
2022-01-22 00:33:47 +0100

    Sexto commit

--- src/main/java/com/mycompany/app/Prueba3.java
+++ src/main/java/com/mycompany/app/Prueba3.java
@@ -1,4 +1,19 @@
 package com.mycompany.app;
 
 public class Prueba3 {
+
+    String nombrePrueba;
+
+    public Prueba(String prueba) {
+        nombrePrueba = prueba;
+    }
+
+    public String getNombrePrueba() {
+        return this.nombrePrueba;
+    }
+
+    public void setNombrePrueba(String prueba) {
+        this.nombrePrueba = prueba;
+        System.out.println("El nombre de la prueba se ha cambiado correctamente. Nuevo nombre: " + prueba);
+    }
 }
