e3d4d9eda66513889ebdc0714fbfc5bba92fcdbf
Eva Manrique <eva.manriquesz@gmail.com>
2022-01-22 00:22:44 +0100

    Cuarto commit

--- src/main/java/com/mycompany/app/Prueba2.java
+++ src/main/java/com/mycompany/app/Prueba2.java
@@ -2,5 +2,19 @@ package com.mycompany.app;
 
 public class Prueba2 {
 
+    String nombrePrueba;
 
+    public Prueba(String prueba) {
+        nombrePrueba = prueba;
+    }
+
+    public String getNombrePrueba() {
+        return this.nombrePrueba;
+    }
+
+    public void setNombrePrueba(String prueba) {
+        this.nombrePrueba = prueba;
+        System.out.println("El nombre de la prueba se ha cambiado correctamente. Nuevo nombre: " + prueba);
+    }
 }
+
