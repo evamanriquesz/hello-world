5bd1ced0266e17fea9a74f6b3bf3e3fe1df48fe5
Eva Manrique <eva.manriquesz@gmail.com>
2022-01-22 00:14:10 +0100

    Tercer commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,15 +1,33 @@
 package com.mycompany.app;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App
 {
+    Prueba p;
+    String nombre;
+
+    public App() {
+        p = new Prueba("Intento");
+        System.out.println("Introduzca un nuevo nombre");
+        Scanner teclado = new Scanner(System.in);
+        nombre = teclado.nextLine();
+        p.setNombrePrueba(nombre);
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         System.out.println("Esta es la práctica 1.");
         System.out.println("Probando commit.");
+        new App();
+
+
+
+
     }
 }
--- src/main/java/com/mycompany/app/Prueba.java
+++ src/main/java/com/mycompany/app/Prueba.java
@@ -2,4 +2,22 @@ package com.mycompany.app;
 
 public class Prueba {
 
+    String nombrePrueba;
+
+    public Prueba(String prueba)
+    {
+        nombrePrueba = prueba;
+    }
+
+    public String getNombrePrueba()
+    {
+        return this.nombrePrueba;
+    }
+
+    public void setNombrePrueba(String prueba)
+    {
+        this.nombrePrueba = prueba;
+        System.out.println("El nombre de la prueba se ha cambiado correctamente. Nuevo nombre: " + prueba);
+    }
+
 }
